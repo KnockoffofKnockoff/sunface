33276c6bddb7f48cdd1ba97be405a922aff0dbe8
Isaac <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-06-20 19:14:51 -0400

    fixed naming

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("HOI4 Modding")
 
-btn_top_left = tk.Button(root, text="Create Country")
-btn_top_left.grid(row=0, column=0, padx=10, pady=10)
+create_country = tk.Button(root, text="Create Country")
+create_country.grid(row=0, column=0, padx=10, pady=10)
 
-btn_top_right = tk.Button(root, text="Create Focus Tree")
-btn_top_right.grid(row=0, column=1, padx=10, pady=10)
+create_focus_tree = tk.Button(root, text="Create Focus Tree")
+create_focus_tree.grid(row=0, column=1, padx=10, pady=10)
 
 root.mainloop()
