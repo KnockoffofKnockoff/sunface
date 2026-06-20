4c0a88c84b80a9f09988d25cf096dbcc579c4747
Isaac <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-06-20 19:46:27 -0400

    working on objects and the translator

--- src/objects.py
+++ src/objects.py
@@ -1,6 +1,14 @@
 # common types
+class Color:
+    def __init__(self, r: int, g: int, b: int):
+        self.r = r
+        self.g = g
+        self.b = b
+
+
 class Country:
-    ...
+    def __init__(self, *, color: Color):
+        self.color = color
 
 
 class FocusTree:
--- src/translator.py
+++ src/translator.py
@@ -2,9 +2,11 @@
 from objects import Country
 
 
-def write_country(country: Country):
+def write_country(country: Country) -> str:
+    # returns the tag used
     ...
 
 
-def read_country(definition_path, history_path):
+def read_country(tag) -> Country:
+    # returns the read country object
     ...
