334432f7bbdb391e9a3c38f74860795956baaa94
Isaac <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-06-20 21:39:37 -0400

    trying yet more to decipher paradox's strange engine

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from objects import Color
 
 root = tk.Tk()
 root.title("HOI4 Modding")
@@ -8,5 +9,7 @@ create_country.grid(row=0, column=0, padx=10, pady=10)
 
 create_focus_tree = tk.Button(root, text="Create Focus Tree")
 create_focus_tree.grid(row=0, column=1, padx=10, pady=10)
+
+print(Color(1, 2, 3))
 
 root.mainloop()
--- src/objects.py
+++ src/objects.py
@@ -1,14 +1,30 @@
 # common types
 class Color:
     def __init__(self, r: int, g: int, b: int):
+        # TODO: bounds checking
         self.r = r
         self.g = g
         self.b = b
 
+    def __str__(self) -> str:
+        return f"{{ {self.r} {self.g} {self.b} }}"
+
+
+# .mod settings as a class
+class Mod:
+    ...
+
 
 class Country:
-    def __init__(self, *, color: Color):
+    def __init__(self, *, name, tag=None, color=Color(255, 0, 255)):
+        self.name = name
+        self.tag = tag if tag is not None else name[:3].upper()
+
         self.color = color
+        # unimplemented stuff (TODO)
+        use_legacy_ai_pp_spend = False
+        graphical_culture = 0
+        graphical_culture_2d = 0
 
 
 class FocusTree:
